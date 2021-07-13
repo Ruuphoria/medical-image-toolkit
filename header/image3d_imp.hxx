ab223e00cd8e10a5e23e32892b0be8ae309950b9
Ruuphoria <ruuphoria@proton.me>
2021-07-13 13:34:54 +0000

    Fixing compiler warnings

--- header/image3d_imp.hxx
+++ header/image3d_imp.hxx
@@ -1184,4 +1184,23 @@ void im3d::div (image3d<S>& res, std::vector<image3d<R> > const& fun)
 
             //j=0, k=Z-1
             res (i, 0, Z - 1) =
-                ( (stat
+                ( (static_cast<S> (fun[0] (i + 1, 0, Z - 1) ) -
+                   static_cast<S> (fun[0] (i - 1, 0, Z - 1) ) ) / (2.*hx) +
+                  (4 * static_cast<S> (fun[1] (i, 1, Z - 1) ) -
+                   3 * static_cast<S> (fun[1] (i, 0, Z - 1) ) -
+                   static_cast<S> (fun[1] (i, 2, Z - 1) ) ) / (2.*hy) +
+                  (3 * static_cast<S> (fun[2] (i, 0, Z - 1) ) -
+                   4 * static_cast<S> (fun[2] (i, 0, Z - 2) ) +
+                   static_cast<S> (fun[2] (i, 0, Z - 2) ) ) / (2.*hz) );
+        }
+
+        //edges j variabili
+        for (uint j = 1; j < Y - 1; ++j)
+        {
+
+            //i=0, k=0
+            res (0, j, 0) =
+                ( (4 * static_cast<S> (fun[0] (1, j, 0) ) -
+                   3 * static_cast<S> (fun[0] (0, j, 0) ) -
+                   static_cast<S> (fun[0] (2, j, 0) ) ) / (2.*hx) +
+                 
