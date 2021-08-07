d8c041a5a7e4351e3c941f77835715db318db4f0
Ruuphoria <ruuphoria@proton.me>
2021-08-07 19:31:51 +0000

    ALSA: atiixp: Fix unlocked snd_pcm_stop() call

--- header/image3d_imp.hxx
+++ header/image3d_imp.hxx
@@ -1938,4 +1938,19 @@ void im3d::image3d<T>::connected_component (image3d<S>& res, image3d<S>& bw,
                         {
                             // west
                             res (i - 1, j, k) = 1 * bw (i - 1, j, k);
-                          
+                            // north
+                            res (i, j + 1, k) = 1 * bw (i, j + 1, k);
+                            // up
+                            res (i, j, k + 1) = 1 * bw (i, j, k + 1);
+                            if (full_connected)
+                            {
+                                // north-west
+                                res (i - 1, j + 1, k) = 1 * bw (i - 1, j + 1, k);
+                                // up-west
+                                res (i - 1, j, k + 1) = 1 * bw (i - 1, j, k + 1);
+                                // up-north
+                                res (i, j + 1, k + 1) = 1 * bw (i, j + 1, k + 1);
+                                // up-north-west
+                                res (i - 1, j + 1, k + 1) = 1 * bw (i - 1, j + 1, k + 1);
+                            }
+         
