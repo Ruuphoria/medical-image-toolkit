83a0a88eb5e10b34efc7ea92fe5428a43ac777b4
Ruuphoria <ruuphoria@proton.me>
2021-09-20 08:04:00 +0000

    trivial changes

--- header/image3d_imp.hxx
+++ header/image3d_imp.hxx
@@ -2189,4 +2189,31 @@ void im3d::image3d<T>::median_filter (image3d<T>& res, int const& radius) const
                 for (int i = -radius; i < radius + 1; ++i)
                     for (int j = -radius; j < radius + 1; ++j)
                     {
-                        mask.push_back ( (*this) (dimx - 1 - std::abs 
+                        mask.push_back ( (*this) (dimx - 1 - std::abs (dimx - (I + i) ), J + j, 0) );
+                    }
+
+                sort ( mask.begin() , mask.end() );
+                res (I, J, 0) = mask[dimmask / 2];
+                mask.resize (0);
+            }
+
+        // south
+        for (int I = radius; I < dimx - radius; ++I)
+            for (int J = 0; J < radius; ++J)
+            {
+                for (int i = -radius; i < radius + 1; ++i)
+                    for (int j = -radius; j < radius + 1; ++j)
+                    {
+                        mask.push_back ( (*this) (I + i, std::abs (J + j), 0) );
+                    }
+
+                sort ( mask.begin() , mask.end() );
+                res (I, J, 0) = mask[dimmask / 2];
+                mask.resize (0);
+            }
+
+        // west
+        for (int I = 0; I < radius; ++I)
+            for (int J = radius; J < dimy - radius; ++J)
+            {
+                for (int i = -r
