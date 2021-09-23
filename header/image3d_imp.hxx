ea612d63b53656c863e2e131e5b5444fef404190
Ruuphoria <ruuphoria@proton.me>
2021-09-23 21:17:41 +0000

    more formatting changes

--- header/image3d_imp.hxx
+++ header/image3d_imp.hxx
@@ -2306,4 +2306,25 @@ void im3d::image3d<T>::median_filter (image3d<T>& res, int const& radius) const
                 for (int K = radius; K < dimz - radius; ++K)
                 {
                     for (int i = -radius; i < radius + 1; ++i)
-                        for (int j = -radius; j < radius + 1; ++
+                        for (int j = -radius; j < radius + 1; ++j)
+                            for (int k = -radius; k < radius + 1; ++k)
+                            {
+                                mask.push_back ( (*this) (I + i, J + j, K + k) );
+                            }
+
+                    sort ( mask.begin() , mask.end() );
+                    res (I, J, K) = mask[dimmask / 2];
+                    mask.resize (0);
+                }
+
+        // 6 faces
+        // down
+        for (int I = radius; I < dimx - radius; ++I)
+            for (int J = radius; J < dimy - radius; ++J)
+                for (int K = 0; K < radius; ++K)
+                {
+                    for (int i = -radius; i < radius + 1; ++i)
+                        for (int j = -radius; j < radius + 1; ++j)
+                            for (int k = -radius; k < radius + 1; ++k)
+                            {
+                        
