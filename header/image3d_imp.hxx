839b0a052af9acb8400beb9b3a94b81c166a5f81
Ruuphoria <ruuphoria@proton.me>
2021-09-29 17:37:04 +0000

    Minor JavaDoc fix.

--- header/image3d_imp.hxx
+++ header/image3d_imp.hxx
@@ -2406,4 +2406,26 @@ void im3d::image3d<T>::median_filter (image3d<T>& res, int const& radius) const
         // east
         for (int I = dimx - radius; I < dimx; ++I)
             for (int J = radius; J < dimy - radius; ++J)
-                for (int K = radiu
+                for (int K = radius; K < dimz - radius; ++K)
+                {
+                    for (int i = -radius; i < radius + 1; ++i)
+                        for (int j = -radius; j < radius + 1; ++j)
+                            for (int k = -radius; k < radius + 1; ++k)
+                            {
+                                mask.push_back ( (*this) (dimx - 1 - std::abs (dimx - (I + i) ), J + j, K + k) );
+                            }
+
+                    sort ( mask.begin() , mask.end() );
+                    res (I, J, K) = mask[dimmask / 2];
+                    mask.resize (0);
+                }
+
+        // 12 adges
+
+        // 4 down
+        // south
+        for (int I = radius; I < dimx - radius; ++I)
+            for (int J = 0; J < radius; ++J)
+                for (int K = 0; K < radius; ++K)
+                {
+                    for (int i = -radius; i < radius + 1; 
