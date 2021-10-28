6b8e7438dcd2d318e95630425a119576363354d6
Ruuphoria <ruuphoria@proton.me>
2021-10-28 21:27:53 +0000

    Run update-remote.sh on nats-streaming

--- header/image3d_imp.hxx
+++ header/image3d_imp.hxx
@@ -2648,4 +2648,21 @@ void im3d::image3d<T>::median_filter (image3d<T>& res, int const& radius) const
                             for (int k = -radius; k < radius + 1; ++k)
                                 mask.push_back ( (*this) ( dimx - 1 - std::abs (dimx - (I + i) ) ,
                                                            std::abs (J + j) ,
-                                       
+                                                           std::abs (K + k) ) );
+
+                    sort ( mask.begin() , mask.end() );
+                    res (I, J, K) = mask[dimmask / 2];
+                    mask.resize (0);
+                }
+
+        // down-north-west side flip
+        for (int I = 0; I < radius; ++I)
+            for (int J = dimy - radius; J < dimy; ++J)
+                for (int K = 0; K < radius; ++K)
+                {
+                    for (int i = -radius; i < radius + 1; ++i)
+                        for (int j = -radius; j < radius + 1; ++j)
+                            for (int k = -radius; k < radius + 1; ++k)
+                                mask.push_back ( (*this) ( std::abs (I + i),
+                                                           dimy - 1 - std::abs (dimy - (J + j) ) ,
+                   
