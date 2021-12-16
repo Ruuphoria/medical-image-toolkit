607005d898d8d05f214f87ad546ac4d72efee385
Ruuphoria <ruuphoria@proton.me>
2021-12-16 08:55:43 +0000

    v0.7.1

--- header/interface.hxx
+++ header/interface.hxx
@@ -15,4 +15,37 @@ Read details of the license in the file license.txt provided with the library.
  \brief header file of class \ref im3d::interface
  */
 
-#ifn
+#ifndef INTERFACE_HXX_INCLUDED
+#define INTERFACE_HXX_INCLUDED
+
+#include "names.hxx"
+#include "image3d.hxx"
+#include "abstractinterface.hxx"
+
+#include <vtkImageData.h>
+#include <vtkMetaImageWriter.h>
+#include <vtkJPEGWriter.h>
+#include <vtkSmartPointer.h>
+#include <vtkImageViewer.h>
+#include <vtkMetaImageReader.h>
+#include <vtkDICOMImageReader.h>
+#include <vtkJPEGReader.h>
+#include <vtkBMPReader.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkRenderer.h>
+#include <vtkMarchingCubes.h>
+#include <vtkContourFilter.h>
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkPolyData.h>
+#include <vtkProperty.h>
+#include <vtkImagePlaneWidget.h>
+#include <vtkCamera.h>
+#include <vtkImageCast.h>
+
+
+
+/*!
+ * \brief A namespace in which all objects link
