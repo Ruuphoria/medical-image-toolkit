f49d7a1863c9edd87bf1c013cd6c62a713450328
Ruuphoria <ruuphoria@proton.me>
2022-01-10 12:46:07 +0000

    VDSM

--- header/interface_imp.hxx
+++ header/interface_imp.hxx
@@ -342,3 +342,588 @@ void im3d::interface<S>::get_coordinates (uint& i, uint& j, uint& k)
         i = dims[0] - 1;
     }
     if ( static_cast<int> (j) >= dims[1] )
+    {
+        j = dims[1] - 1;
+    }
+    if ( static_cast<int> (k) >= dims[2] )
+    {
+        k = dims[2] - 1;
+    }
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::get_coordinates (double& x1, double& y1, double& z1,
+                                          double& x2, double& y2, double& z2)
+{
+    int dims[3], dimz;
+
+    this->imageptr->GetDimensions (dims);
+    dimz = dims[2];
+
+    std::cout << "Showing your image ..." << std::endl;
+    std::cout << "Click with mouse on the image to know coordinates." << std::endl;
+    std::cout << "When you have chosen a pair of pixels write ";
+    std::cout << "coordinates (positive) in a piece of paper and close the image.";
+    std::cout << std::endl << std::endl;
+
+    this->show_image();
+
+    // 2d case
+    if (dimz == 1)
+    {
+        std::cout << "Write coordinates of the first pixel (x1,y1):" << std::endl;
+
+        do
+        {
+            std::cout << "\tx1: ";
+            cin >> x1;
+        }
+        while (x1 < 0);
+
+        do
+        {
+            std::cout << "\ty1: ";
+            cin >> y1;
+        }
+        while (y1 < 0);
+
+        std::cout << "Write coordinates of the second pixel (x2,y2):" << std::endl;
+
+        do
+        {
+            std::cout << "\tx2: ";
+            cin >> x2;
+        }
+        while (x2 < 0);
+
+        do
+        {
+            std::cout << "\ty2: ";
+            cin >> y2;
+        }
+        while (y2 < 0);
+
+        z1 = z2 = 0;
+    }
+    // 3d case
+    else
+    {
+        std::cout << "Write coordinates of the first pixel (x1,y1,z1):" << std::endl;
+
+        do
+        {
+            std::cout << "\tx1: ";
+            cin >> x1;
+        }
+        while (x1 < 0);
+
+        do
+        {
+            std::cout << "\ty1: ";
+            cin >> y1;
+        }
+        while (y1 < 0);
+
+        do
+        {
+            std::cout << "\tz1: ";
+            cin >> z1;
+        }
+        while (z1 < 0);
+
+        std::cout << "Write coordinates of the second pixel (x2,y2,z2):" << std::endl;
+
+        do
+        {
+            std::cout << "\tx2: ";
+            cin >> x2;
+        }
+        while (x2 < 0);
+
+        do
+        {
+            std::cout << "\ty2: ";
+            cin >> y2;
+        }
+        while (y2 < 0);
+
+        do
+        {
+            std::cout << "\tz2: ";
+            cin >> z2;
+        }
+        while (z2 < 0);
+    }
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::get_coordinates (uint& i1, uint& j1, uint& k1,
+                                          uint& i2, uint& j2, uint& k2)
+{
+    double x1, y1, z1, x2, y2, z2;
+    int dims[3];
+    double h[3];
+
+    this->imageptr->GetDimensions (dims);
+    this->imageptr->GetSpacing (h);
+
+    this->get_coordinates (x1, y1, z1, x2, y2, z2);
+
+    i1 = floor (x1 / h[0]);
+    i2 = floor (x2 / h[0]);
+    j1 = floor (y1 / h[1]);
+    j2 = floor (y2 / h[1]);
+
+    if (dims[2] == 1)
+    {
+        k1 = k2 = 0;
+    }
+    else
+    {
+        k1 = floor (z1 / h[2]);
+        k2 = floor (z2 / h[2]);
+    }
+
+    if ( static_cast<int> (i1) >= dims[0] )
+    {
+        i1 = dims[0] - 1;
+    }
+    if ( static_cast<int> (j1) >= dims[1] )
+    {
+        j1 = dims[1] - 1;
+    }
+    if ( static_cast<int> (k1) >= dims[2] )
+    {
+        k1 = dims[2] - 1;
+    }
+
+    if ( static_cast<int> (i2) >= dims[0] )
+    {
+        i2 = dims[0] - 1;
+    }
+    if ( static_cast<int> (j2) >= dims[1] )
+    {
+        j2 = dims[1] - 1;
+    }
+    if ( static_cast<int> (k2) >= dims[2] )
+    {
+        k2 = dims[2] - 1;
+    }
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::show_image_and_contour (double const& level)
+{
+    int dims[3], dimz;
+
+    this->imageptr->GetDimensions (dims);
+    dimz = dims[2];
+
+    vtkSmartPointer<vtkRenderer> renderer = NULL;
+    vtkSmartPointer<vtkRenderWindowInteractor> renWinInt = NULL;
+
+    newrendering (renderer, renWinInt);
+
+    setopacity();
+    setcolour();
+    addcontour2renderer (renderer, level);
+
+    vtkSmartPointer<vtkRenderWindow> renWin = vtkRenderWindow::New();
+
+    renWin->SetInteractor (renWinInt);
+    renWin->AddRenderer (renderer);
+
+    vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetZ = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+    imagePlaneWidgetZ->SetInputData (this->imageptr);
+#else
+    imagePlaneWidgetZ->SetInput (this->imageptr);
+#endif
+    imagePlaneWidgetZ->SetInteractor (renWinInt);
+    imagePlaneWidgetZ->SetPlaneOrientationToZAxes();
+    imagePlaneWidgetZ->DisplayTextOn();
+    imagePlaneWidgetZ->UseContinuousCursorOn();
+    imagePlaneWidgetZ->On();
+
+    if (dimz != 1)
+    {
+        vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetX = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+        imagePlaneWidgetX->SetInputData (this->imageptr);
+#else
+        imagePlaneWidgetX->SetInput (this->imageptr);
+#endif
+        imagePlaneWidgetX->SetInteractor (renWinInt);
+        imagePlaneWidgetX->SetPlaneOrientationToXAxes();
+        imagePlaneWidgetX->DisplayTextOn();
+        imagePlaneWidgetX->UseContinuousCursorOn();
+        imagePlaneWidgetX->On();
+
+        vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetY = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+        imagePlaneWidgetY->SetInputData (this->imageptr);
+#else
+        imagePlaneWidgetY->SetInput (this->imageptr);
+#endif
+        imagePlaneWidgetY->SetInteractor (renWinInt);
+        imagePlaneWidgetY->SetPlaneOrientationToYAxes();
+        imagePlaneWidgetY->DisplayTextOn();
+        imagePlaneWidgetY->UseContinuousCursorOn();
+        imagePlaneWidgetY->On();
+    }
+
+    interface::startshowing (renderer, renWinInt);
+
+    setopacity();
+    setcolour();
+
+    deleterendering (renderer, renWinInt);
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::show_contour_with_background_image (interface& backgroundimage,
+                                                             double const& level)
+{
+    int dims[3], dimz;
+
+    this->imageptr->GetDimensions (dims);
+    dimz = dims[2];
+
+    vtkSmartPointer<vtkRenderer> renderer = NULL;
+    vtkSmartPointer<vtkRenderWindowInteractor> renWinInt = NULL;
+
+    newrendering (renderer, renWinInt);
+
+    setopacity();
+    setcolour();
+    addcontour2renderer (renderer, level);
+
+    vtkSmartPointer<vtkRenderWindow> renWin = vtkRenderWindow::New();
+
+    renWin->SetInteractor (renWinInt);
+    renWin->AddRenderer (renderer);
+
+    vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetZ = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+    imagePlaneWidgetZ->SetInputData (backgroundimage.getimageptr() );
+#else
+    imagePlaneWidgetZ->SetInput (backgroundimage.getimageptr() );
+#endif
+    imagePlaneWidgetZ->SetInteractor (renWinInt);
+    imagePlaneWidgetZ->SetPlaneOrientationToZAxes();
+    imagePlaneWidgetZ->DisplayTextOn();
+    imagePlaneWidgetZ->UseContinuousCursorOn();
+    imagePlaneWidgetZ->On();
+
+    if (dimz != 1)
+    {
+        vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetX = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+        imagePlaneWidgetX->SetInputData (backgroundimage.getimageptr() );
+#else
+        imagePlaneWidgetX->SetInput (backgroundimage.getimageptr() );
+#endif
+        imagePlaneWidgetX->SetInteractor (renWinInt);
+        imagePlaneWidgetX->SetPlaneOrientationToXAxes();
+        imagePlaneWidgetX->DisplayTextOn();
+        imagePlaneWidgetX->UseContinuousCursorOn();
+        imagePlaneWidgetX->On();
+
+        vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetY = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+        imagePlaneWidgetY->SetInputData (backgroundimage.getimageptr() );
+#else
+        imagePlaneWidgetY->SetInput (backgroundimage.getimageptr() );
+#endif
+        imagePlaneWidgetY->SetInteractor (renWinInt);
+        imagePlaneWidgetY->SetPlaneOrientationToYAxes();
+        imagePlaneWidgetY->DisplayTextOn();
+        imagePlaneWidgetY->UseContinuousCursorOn();
+        imagePlaneWidgetY->On();
+    }
+
+    interface::startshowing (renderer, renWinInt);
+
+    setopacity();
+    setcolour();
+
+    deleterendering (renderer, renWinInt);
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::write (std::string const& imagename, std::string const& extension) const
+{
+    if (extension == ".mhd")
+    {
+        vtkSmartPointer<vtkMetaImageWriter> metaWriter = vtkMetaImageWriter::New();
+        metaWriter->SetFileName ( (imagename + extension).c_str() );
+        metaWriter->SetRAWFileName ( (imagename + ".raw").c_str() );
+#if VTK_VERSION_MAJOR >= 6
+        metaWriter->SetInputData (this->imageptr);
+#else
+        metaWriter->SetInput (this->imageptr);
+#endif
+        metaWriter->SetCompression (false);
+        metaWriter->Write();
+    }
+    else if (extension == ".jpg")
+    {
+        vtkSmartPointer<vtkImageCast> castFilter = vtkSmartPointer<vtkImageCast>::New();
+        castFilter->SetOutputScalarTypeToUnsignedChar ();
+#if VTK_VERSION_MAJOR >= 6
+        castFilter->SetInputData (this->imageptr);
+#else
+        castFilter->SetInput (this->imageptr);
+#endif
+        castFilter->Update();
+
+        vtkSmartPointer<vtkJPEGWriter> writer = vtkSmartPointer<vtkJPEGWriter>::New();
+        writer->SetFileName ( (imagename + extension).c_str() );
+        writer->SetInputConnection (castFilter->GetOutputPort() );
+        writer->Write();
+    }
+    else
+    {
+        std::cout << "ERROR: " << imagename << " has an extension " << extension;
+        std::cout << " not allowed in im3d::interface<S>::write" << std::endl;
+    }
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::addcontour2renderer (vtkSmartPointer<vtkRenderer>& renderer,
+                                              double const& level)
+{
+    int dims[3], dimz;
+
+    this->imageptr->GetDimensions (dims);
+    dimz = dims[2];
+
+    vtkSmartPointer<vtkPolyData> surface;
+
+    // 3d case
+    if (dimz != 1)
+    {
+
+        vtkSmartPointer<vtkMarchingCubes> marchingCubes = vtkMarchingCubes::New();
+
+#if VTK_VERSION_MAJOR >= 6
+        marchingCubes->SetInputData (this->imageptr);
+#else
+        marchingCubes->SetInput (this->imageptr);
+#endif
+        marchingCubes->SetValue (0, level);
+        marchingCubes->Update();
+
+        surface = marchingCubes->GetOutput();
+    }
+
+    // 2d case
+    else
+    {
+        setcolour (1., 0., 0.);
+        setopacity (1.);
+
+        vtkSmartPointer<vtkContourFilter> contour = vtkContourFilter::New();
+
+#if VTK_VERSION_MAJOR >= 6
+        contour->SetInputData (this->imageptr);
+#else
+        contour->SetInput (this->imageptr);
+#endif
+        contour->SetValue (0, level);
+        contour->Update();
+
+        surface = contour->GetOutput();
+    }
+
+    vtkSmartPointer<vtkPolyDataMapper> mapper = vtkPolyDataMapper::New();
+#if VTK_VERSION_MAJOR >= 6
+    mapper->SetInputData (surface);
+#else
+    mapper->SetInput (surface);
+#endif
+    mapper->ScalarVisibilityOff();
+
+    vtkSmartPointer<vtkActor> actor = vtkActor::New();
+
+    actor->SetMapper (mapper);
+    actor->GetProperty()->SetOpacity (opacity);
+    actor->GetProperty()->SetColor (colour[0], colour[1], colour[2]);
+
+    renderer->AddActor (actor);
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::addcontour2renderer (double const& level,
+                                              vtkSmartPointer<vtkRenderer>& renderer)
+{
+    addcontour2renderer (renderer, level);
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::addplanes2renderwindowinteractor
+(vtkSmartPointer<vtkRenderer>& renderer,
+ vtkSmartPointer<vtkRenderWindowInteractor>& renWinInt) const
+{
+    int dims[3], dimz;
+
+    this->imageptr->GetDimensions (dims);
+    dimz = dims[2];
+
+    vtkSmartPointer<vtkRenderWindow> renWin = vtkRenderWindow::New();
+
+    renWin->SetInteractor (renWinInt);
+    renWin->AddRenderer (renderer);
+
+    vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetZ = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+    imagePlaneWidgetZ->SetInputData (this->imageptr);
+#else
+    imagePlaneWidgetZ->SetInput (this->imageptr);
+#endif
+    imagePlaneWidgetZ->SetInteractor (renWinInt);
+    imagePlaneWidgetZ->SetPlaneOrientationToZAxes();
+    imagePlaneWidgetZ->DisplayTextOn();
+    imagePlaneWidgetZ->UseContinuousCursorOn();
+    imagePlaneWidgetZ->On();
+
+    if (dimz != 1)
+    {
+        vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetX = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+        imagePlaneWidgetX->SetInputData (this->imageptr);
+#else
+        imagePlaneWidgetX->SetInput (this->imageptr);
+#endif
+        imagePlaneWidgetX->SetInteractor (renWinInt);
+        imagePlaneWidgetX->SetPlaneOrientationToXAxes();
+        imagePlaneWidgetX->DisplayTextOn();
+        imagePlaneWidgetX->UseContinuousCursorOn();
+        imagePlaneWidgetX->On();
+
+        vtkSmartPointer<vtkImagePlaneWidget> imagePlaneWidgetY = vtkImagePlaneWidget::New();
+#if VTK_VERSION_MAJOR >= 6
+        imagePlaneWidgetY->SetInputData (this->imageptr);
+#else
+        imagePlaneWidgetY->SetInput (this->imageptr);
+#endif
+        imagePlaneWidgetY->SetInteractor (renWinInt);
+        imagePlaneWidgetY->SetPlaneOrientationToYAxes();
+        imagePlaneWidgetY->DisplayTextOn();
+        imagePlaneWidgetY->UseContinuousCursorOn();
+        imagePlaneWidgetY->On();
+    }
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::newrendering
+(vtkSmartPointer<vtkRenderer>& renderer,
+ vtkSmartPointer<vtkRenderWindowInteractor>& renWinInt)
+{
+    renWinInt = vtkRenderWindowInteractor::New();
+    renderer = vtkRenderer::New();
+
+    //renWinInt.SetInteractorStyle(vtkInteractorStyleTrackballCamera::New());
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::deleterendering
+(vtkSmartPointer<vtkRenderer>& renderer,
+ vtkSmartPointer<vtkRenderWindowInteractor>& renWinInt)
+{
+    renderer->Delete();
+    renWinInt->Delete();
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::reset (vtkSmartPointer<vtkRenderer>& renderer,
+                                vtkSmartPointer<vtkRenderWindowInteractor>& renWinInt)
+{
+    deleterendering (renderer, renWinInt);
+    newrendering (renderer, renWinInt);
+
+    return;
+}
+
+
+
+template <typename S>
+void im3d::interface<S>::startshowing (vtkSmartPointer<vtkRenderer>& renderer,
+                                       vtkSmartPointer<vtkRenderWindowInteractor>& renWinInt,
+                                       int const& windowwidth, int const& windowheight)
+{
+    vtkSmartPointer<vtkRenderWindow> renWin = vtkRenderWindow::New();
+
+    renWin->SetInteractor (renWinInt);
+    renWin->AddRenderer (renderer);
+    renWin->SetSize (windowwidth, windowheight);
+    renderer->SetBackground (0.25, 0.25, 0.75);
+
+    vtkSmartPointer<vtkInteractorStyleTrackballCamera> trackStyle = vtkInteractorStyleTrackballCamera::New();
+    renWinInt->SetInteractorStyle (trackStyle);
+
+    renWinInt->Initialize();
+    renWin->Render();
+    renWinInt->Start();
+
+    return;
+}
+
+
+template <typename S>
+void im3d::interface<S>::startshowing (int const& windowwidth, int const& windowheight,
+                                       vtkSmartPointer<vtkRenderer>& renderer,
+                                       vtkSmartPointer<vtkRenderWindowInteractor>& renWinInt)
+{
+    startshowing (renderer, renWinInt, windowwidth, windowheight);
+
+    return;
+}
+
+
+
+#endif // INTERFACE_IMP_HXX_INCLUDED
