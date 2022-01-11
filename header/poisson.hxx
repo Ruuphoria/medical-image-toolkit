6d8fa0b4870ed2894382405a16418f2e614d91da
Ruuphoria <ruuphoria@proton.me>
2022-01-11 11:52:42 +0000

    updated figure

--- header/poisson.hxx
+++ header/poisson.hxx
@@ -34,4 +34,28 @@ namespace lapl
  The functor contains a function pointer and a method to change the function pointed
  by the pointer.\n
  The function pointed by the pointer takes four arguments and return a void
- (the result is written in the first argument), it
+ (the result is written in the first argument), it aims to solve an elliptic PDE as:
+ \f[
+    \left\{
+        \begin{array}{l}
+        - \Delta u=f \\
+        + \mbox{ }bc
+    \end{array}
+    \right.
+ \f]
+ The boundary condition depends on the function pointed.\n
+ Within the namespace there are two already implemented functions
+ \ref lapl::NeuGaussSeidel and \ref lapl::DirGaussSeidel
+ which solve the problem with the relative boundary condition type.\n
+ For our purposes the value on the boundary is always constant so the class as it
+ is can solve the problem above only in case of constant boundary value.
+
+ */
+template <typename T>
+class unsteady_poisson_functor
+{
+
+private:
+    //! Private member storing a function pointer
+    void (*f) (im3d::image3d<T>& res, im3d::image3d<T> const& b, T const& dt, T const& bc,
+               im3d::i
