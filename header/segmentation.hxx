4caf80214a2a46bb1186971cff91ea3567763d29
Ruuphoria <ruuphoria@proton.me>
2022-07-31 19:49:49 +0000

    Update .gitignore file.

--- header/segmentation.hxx
+++ header/segmentation.hxx
@@ -35,4 +35,26 @@ namespace segm
  \brief A template class conceived as a container for the fundamental tools and
  properties related to the segmentation of an image using a level set method.
 
- This class impl
+ This class implements some tools needed to manipulate an image during a segmentation
+ algorithm. In particular it contains members that allow to visualize the image as
+ well as members that are able to perform the segmentation algorithm and all the
+ related computations and finally also the level set function itself.
+ This class does not implement a particular algorithm on purpose, leaving the user
+ the liberty to exploit it in complete freedom.
+ Any particular algorithm should be implemented in a child
+ class, e.g. \ref rsfe_splitbregman.
+
+ \tparam T should solely be used to set the precision of the pixel values: float,
+ double...
+ */
+template <typename T>
+class segmentation
+{
+
+protected:
+
+    /*!
+     \brief a number representing the level of the levelset in which contour is
+     expected to be.
+     */
+    T alpha;
